bdd1a7e62e68d3eb19988ec4f7332a9ece9ab954
dayoungh <dayeong1024@gmail.com>
2024-08-21 16:54:40 +0900

    [황다영] movie API, weather 마크업 수정

--- src/movies.ts
+++ src/movies.ts
@@ -1,8 +1,39 @@
 import "./main";
 import "./styles/movie.scss";
+import { MovieListService } from './service/movies.service'
 
 
-//필요 API
-// 1. 현재 상영중인 영화 조회
-// 2. 영화 장르 조회
-// 3. 개봉 예정 영화
+const movieListService = new MovieListService();
+
+
+// https://image.tmdb.org/t/p/w500 이미지 url 
+
+
+
+// (async () => {
+
+//   const nowPlayingData = await movieListService.getNowPlayingMovies({
+//     params: {
+//       include_adult: false,
+//       include_video: false,
+//       language: 'ko-KR',
+//       page: 1,
+//     },
+//   });
+//   console.log('nowplayingdata', nowPlayingData);
+
+
+//   const upcomingData = await movieListService.getUpcomingMovies({
+//     params: {
+//       include_adult: false,
+//       include_video: false,
+//       language: 'ko-KR',
+//       page: 1,
+//     },
+//   });
+//   console.log('upcomingData', upcomingData);
+
+
+// })();
+
+
