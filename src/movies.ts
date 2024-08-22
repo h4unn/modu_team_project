ffe29bbca047b119247c0d6d7b12b28b5b4a2fb4
dayoungh <dayeong1024@gmail.com>
2024-08-22 09:51:47 +0900

    [황다영] movie.ts

--- src/movies.ts
+++ src/movies.ts
@@ -3,7 +3,9 @@ import "./styles/movie.scss";
 import { MovieListService } from './service/movies.service'
 
 
-const movieListService = new MovieListService();
+
+
+// const movieListService = new MovieListService();
 
 
 // https://image.tmdb.org/t/p/w500 이미지 url 
@@ -36,4 +38,32 @@ const movieListService = new MovieListService();
 
 // })();
 
+
+
+//------------------------test 8/22 황다영 ----------------------//
+
+// <nav class="category"></nav>
+const navEl = document.querySelector('.category');
+
+(navEl as HTMLElement).addEventListener('click', (e) => {
+  // 2.
+  const targetEl = e.target as HTMLElement;
+  // const targetEl = e.target as EventTarget;
+  // EventTarget이랑 HTMLElement 어떤차이..? 
+
+   // 1. 실제 이벤트가 발생하는 button을 target 
+  const button = targetEl.closest('button');
+
+  if (button) {
+    (e.currentTarget as HTMLElement)
+    ?.querySelectorAll('button')
+    ?.forEach((button) => {
+      if (button.classList.contains('active')) {
+        button.classList.remove('active');
+      }
+    });
+
+    button.classList.add('active');
+  }
+});
 
