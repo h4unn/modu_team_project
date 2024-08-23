44a88d761a2ba6ac61b66a29301cc941d693e24e
hyunnam-cho <wiwi154@naver.com>
2024-08-23 17:43:16 +0900

    [조현남] movie popup 작업

--- src/moviesElements.ts
+++ src/moviesElements.ts
@@ -3,7 +3,14 @@ const $ = {
   movieContainer: document.querySelector('.movie-container'),
   movieContainer2: document.querySelector('.movie-container-2'),
   movieContainer3: document.querySelector('.movie-container-3'),
- 
+  // dialog Ele
+  movieDialog: document.querySelector('dialog'),
+  movieTit : document.querySelector('.movie_tit h3'),
+  movieOriginTit : document.querySelector('.origin_tit'),
+  movieDesc : document.querySelector('.movie_desc'),
+  movieLike : document.querySelector('.movie_like span'),
+  movieAudience : document.querySelector('.audience span'),
+  movieGrade : document.querySelector('.grade span'),
 } as const;
 
 export default $;
